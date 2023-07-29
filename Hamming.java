e29fa2d664df53aee9b59af130d523d7433333a4
Paola Contreras <paolacontreras231@gmail.com>
2023-07-29 01:53:28 -0600

    first commit

---
+++ Hamming.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Hamming{
+    public static void main (String args[]){
+
+        Scanner myObj = new Scanner(System.in);  
+        System.out.println("Ingrese informacion");
+
+        String Data = myObj.nextLine(); 
+        System.out.println("La data ingresada es: " + Data);  
+
+        i = Data.length();
+        p = 
+
+        // System.out.println();  
+    }
+}
+
+/* 2^p >= p + i + 1 
+ * donde p es el bit de paridad
+ * donde i es la cantidad de bits 
+*/
